fb314b04e1155f4cb7ad9b10bb398d2ceeb9e6dc
YellowGrow <yellowgrow@naver.com>
2023-08-04 09:33:10 +0900

    수렵 오류 수정

--- script/c101275007.lua
+++ script/c101275007.lua
@@ -39,9 +39,9 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and e:GetLabel(0) and tc:IsRelateToEffect(e) and tc:IsLocation(LOCATION_MZONE) and tc:IsFaceup() then
-		Duel.ChangePosition(tc,POS_FACEDOWN_DEFENSE)
-	elseif tc and e:GetLabel(1) and tc:IsRelateToEffect(e) and tc:IsLocation(LOCATION_MZONE) and tc:IsFaceup() then
+	if tc and e:GetLabel()==1 and tc:IsRelateToEffect(e) and tc:IsLocation(LOCATION_MZONE) and tc:IsFaceup() then
 		Duel.GetControl(tc,tp,PHASE_END,1)
+	elseif tc and tc:IsRelateToEffect(e) and tc:IsLocation(LOCATION_MZONE) and tc:IsFaceup() then
+		Duel.ChangePosition(tc,POS_FACEDOWN_DEFENSE)
 	end
 end
