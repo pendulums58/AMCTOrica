ea17f122763901b1485d9a9ecd9b5209503c8440
YellowGrow <yellowgrow@naver.com>
2023-08-04 09:13:43 +0900

    Update c101275007.lua

--- script/c101275007.lua
+++ script/c101275007.lua
@@ -1,5 +1,6 @@
 --수렵구 - 마비덫
-function c101275007.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_CONTROL+CATEGORY_POSITION)
