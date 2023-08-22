bc242e01ff0f3c4d0da3a22a756d33ef121bfaac
YellowGrow <yellowgrow@naver.com>
2023-08-22 20:40:45 +0900

    수렵 길드 코드 수정

--- script/c101275012.lua
+++ script/c101275012.lua
@@ -1,75 +1,60 @@
 --수렵 길드
 local s,id=GetID()
 function s.initial_effect(c)
-   --발동
-   local e1=Effect.CreateEffect(c)
-   e1:SetType(EFFECT_TYPE_ACTIVATE)
-   e1:SetCode(EVENT_FREE_CHAIN)
-   c:RegisterEffect(e1)
-   --토큰 생성
-   local e2=Effect.CreateEffect(c)
-   e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
-   e2:SetType(EFFECT_TYPE_IGNITION)
-   e2:SetRange(LOCATION_FZONE)
-   e2:SetDescription(aux.Stringid(id,0))
-   e2:SetCountLimit(1)
-   e2:SetCondition(s.spcon)
-   e2:SetTarget(s.sptg)
-   e2:SetOperation(s.spop)
-   c:RegisterEffect(e2)
-   --마함존 특소
-   local e3=Effect.CreateEffect(c)
-   e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-   e3:SetType(EFFECT_TYPE_IGNITION)
-   e3:SetRange(LOCATION_FZONE)
-   e3:SetDescription(aux.Stringid(id,1))
-   e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-   e3:SetCountLimit(1)
-   e3:SetTarget(s.sptg1)
-   e3:SetOperation(s.spop1)
-   c:RegisterEffect(e3)
+    --발동
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e1)
+	--내성
+    local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_ONFIELD,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x10cd))
+	e2:SetValue(s.efilter)
+	c:RegisterEffect(e2)
+	--토큰 생성
+    local e3=Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_FZONE)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCountLimit(1)
+    e3:SetCondition(s.spcon)
+    e3:SetTarget(s.sptg)
+    e3:SetOperation(s.spop)
+    c:RegisterEffect(e3)
+end
+function s.efilter(e,te)
+	local lv=c:GetLevel()
+	if c:IsType(TYPE_XYZ) then lv=c:GetRank() end
+	return te:GetOwnerPlayer()~=e:GetHandlerPlayer() and te:IsActiveType(TYPE_MONSTER) and lv>=8
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-   return not Duel.IsExistingMatchingCard(s.spchk,tp,0,LOCATION_MZONE,1,nil)
+    return not Duel.IsExistingMatchingCard(s.spchk,tp,0,LOCATION_MZONE,1,nil)
 end
 function s.spchk(c)
-   local lv=c:GetLevel()
-   if c:IsType(TYPE_XYZ) then lv=c:GetRank() end
-   return lv>=8
+    local lv=c:GetLevel()
+    if c:IsType(TYPE_XYZ) then lv=c:GetRank() end
+    return lv>=8
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-   if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 
-      and Duel.IsPlayerCanSpecialSummonMonster(tp,101275999,0xf,0x4011,0,0,8,RACE_DRAGON,ATTRIBUTE_EARTH,POS_FACEUP,1-tp)end
-   
+    if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 
+        and Duel.IsPlayerCanSpecialSummonMonster(tp,101275999,0xf,0x4011,0,0,8,RACE_DRAGON,ATTRIBUTE_EARTH,POS_FACEUP,1-tp)end
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-   local c=e:GetHandler()
-   if c:IsRelateToEffect(e) and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 then
-      local token=Duel.CreateToken(tp,101275999)
-      Duel.SpecialSummonStep(token,0,tp,1-tp,false,false,POS_FACEUP)
-      local e1=Effect.CreateEffect(c)
-      e1:SetType(EFFECT_TYPE_SINGLE)
-      e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-      e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
-      e1:SetValue(1)
-      token:RegisterEffect(e1)
-      Duel.SpecialSummonComplete()
-   end
-end
-function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-   if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_SZONE) and s.spfilter(chkc,e,tp) end
-   if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-	and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_SZONE,0,1,nil,e,tp) end
-   local tc=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_SZONE,0,1,1,nil,e,tp)
-   Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tc,1,tp,LOCATION_SZONE)
-end
-function s.spop1(e,tp,eg,ep,ev,re,r,rp)
-   local tc=Duel.GetFirstTarget()
-   if tc:IsRelateToEffect(e) and e:GetHandler():IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-      Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-   end
-end
-function s.spfilter(c,e,tp)
-   local ty=c:GetOriginalType()
-   return bit.band(ty,TYPE_MONSTER)==TYPE_MONSTER and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 then
+        local token=Duel.CreateToken(tp,101275999)
+        Duel.SpecialSummonStep(token,0,tp,1-tp,false,false,POS_FACEUP)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+        e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+        e1:SetValue(1)
+        token:RegisterEffect(e1)
+        Duel.SpecialSummonComplete()
+    end
 end
