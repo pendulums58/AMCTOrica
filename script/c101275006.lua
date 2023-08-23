7735884c7a7595532e367d6a582e861badd2f02e
YellowGrow <yellowgrow@naver.com>
2023-08-23 19:20:15 +0900

    수렵 패치

--- script/c101275006.lua
+++ script/c101275006.lua
@@ -1,37 +1,14 @@
 --수렵표적 다라-아마듈라
 local s,id=GetID()
 function s.initial_effect(c)
-	--상대필드 소환 및 1장제약
 	YiPi.HuntTarget(c)
-	--표시형식 변경 불가
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(3313)
-	e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
-	c:RegisterEffect(e2)
-	--표시형식 변경
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_POSITION)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e3:SetCountLimit(1)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetOperation(s.posop)
-	c:RegisterEffect(e3)
 	--필드 수 제한
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e4:SetCode(EFFECT_MAX_MZONE)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetTargetRange(1,1)
-	e4:SetValue(3)
-	c:RegisterEffect(e4)
-end
-function s.posop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsFaceup() and c:IsRelateToEffect(e) then
-		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
-	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_MAX_MZONE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(1,1)
+	e1:SetValue(3)
+	c:RegisterEffect(e1)
 end
