7a6eec2d24d5e42fbc058eb5feefcb8fc8b6bf38
YellowGrow <yellowgrow@naver.com>
2023-08-23 19:10:44 +0900

    다라아마듈라

--- script/c101275006.lua
+++ script/c101275006.lua
@@ -1,51 +1,37 @@
---수렵표적 리오레우스
+--수렵표적 다라-아마듈라
 local s,id=GetID()
 function s.initial_effect(c)
+	--상대필드 소환 및 1장제약
 	YiPi.HuntTarget(c)
+	--표시형식 변경 불가
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetCountLimit(1,{id,1})
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetDescription(3313)
+	e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
 	c:RegisterEffect(e2)
+	--표시형식 변경
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_DESTROYED)
-	e3:SetCountLimit(1,{id,2})
-	e3:SetTarget(s.settg)
-	e3:SetOperation(s.setop)
+	e3:SetCategory(CATEGORY_POSITION)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetCountLimit(1)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetOperation(s.posop)
 	c:RegisterEffect(e3)
+	--필드 수 제한
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetCode(EFFECT_MAX_MZONE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(1,1)
+	e4:SetValue(3)
+	c:RegisterEffect(e4)
 end
-function s.spfilter(c,e,tp)
-	return c:IsSetCard(SETCARD_HUNTER) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,1-tp,false,false)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,0,LOCATION_DECK,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,1-tp,LOCATION_DECK)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(1-tp,LOCATION_MZONE)<=0 then return end
-	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SPSUMMON)
-	local tc=Duel.SelectMatchingCard(1-tp,s.spfilter,tp,0,LOCATION_DECK,1,1,nil,e,tp):GetFirst()
-	if tc then
-		Duel.SpecialSummon(tc,0,1-tp,1-tp,false,false,POS_FACEUP)
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect() then
+		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
 	end
-end
-function s.filter(c)
-	return c:IsSetCard(SETCARD_HUNTTOOL) and c:IsSSetable() and c:IsType(TYPE_SPELL)
-end
-function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
-end
-function s.setop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
-		Duel.SSet(tp,g:GetFirst())
-	end
-end
-function s.thfilter(c)
-	return c:IsFacedown()
 end
