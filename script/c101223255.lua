29577ab0d4db112560d14a2181efc8db59d2c882
시안색새벽 <pendulums@naver.com>
2023-08-24 22:01:30 +0900

    Update c101223255.lua

--- script/c101223255.lua
+++ script/c101223255.lua
@@ -10,7 +10,7 @@ function s.initial_effect(c)
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetTargetRange(LOCATION_MZONE,0)
 	e1:SetValue(1000)
