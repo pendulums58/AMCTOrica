1d970374238ee22ac3664f0726d584e479ee4473
YellowGrow <yellowgrow@naver.com>
2023-08-25 10:05:58 +0900

    Update c101275012.lua

--- script/c101275012.lua
+++ script/c101275012.lua
@@ -26,25 +26,26 @@ function s.atkfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsFaceup()
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) then
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(g,1-tp)
-	end
-	if e:GetLabel()==1 and Duel.IsExistingMatchingCard(s.htfilter,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingMatchingCard(s.atkfilter,tp,0,LOCATION_MZONE,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(0)) then
-		local atkg=Duel.GetMatchingGroup(s.atkfilter,tp,0,LOCATION_MZONE,nil)
-		local tc=Duel.SelectMatchingCard(tp,s.htfilter,tp,LOCATION_MZONE,0,1,1,nil)
-		if #atkg>0 and #tc>0 then
-			local tcc=tc:GetFirst()
-			local tg=atkg:GetMaxGroup(Card.GetAttack)
-			local atk=tg:GetFirst():GetAttack()
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetValue(atk)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			tcc:RegisterEffect(e1,true)
+	if Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) then
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(g,1-tp)
+		end
+		if e:GetLabel()==1 and Duel.IsExistingMatchingCard(s.htfilter,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingMatchingCard(s.atkfilter,tp,0,LOCATION_MZONE,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(0)) then
+			local atkg=Duel.GetMatchingGroup(s.atkfilter,tp,0,LOCATION_MZONE,nil)
+			local tc=Duel.SelectMatchingCard(tp,s.htfilter,tp,LOCATION_MZONE,0,1,1,nil)
+			if #atkg>0 and #tc>0 then
+				local tcc=tc:GetFirst()
+				local tg=atkg:GetMaxGroup(Card.GetAttack)
+				local atk=tg:GetFirst():GetAttack()
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetCode(EFFECT_UPDATE_ATTACK)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetValue(atk)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+				tcc:RegisterEffect(e1,true)
+			end
 		end
 	end
 end
